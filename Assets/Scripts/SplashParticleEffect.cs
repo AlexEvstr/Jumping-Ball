f42dec489beea18b2008a95472e9751dc99570d3
Alexander Evstratov <alexdrugev@me.com>
2023-02-01 11:23:54 +0300

    Added ParticleEffect.

---
+++ Assets/Scripts/SplashParticleEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashParticleEffect : MonoBehaviour
+{
+    public GameObject prefabSplashParticleEffect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject newParticleEffect = Instantiate(prefabSplashParticleEffect);
+        newParticleEffect.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+    }
+}
