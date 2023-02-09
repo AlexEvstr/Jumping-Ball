edcf80f662f532f0a10f7794e0cb5a94cbd8ea40
Alexander Evstratov <alexdrugev@me.com>
2023-02-10 01:34:48 +0300

    Change soundButton.

--- Assets/Scripts/Buttons/UISoundButton.cs
+++ Assets/Scripts/Buttons/UISoundButton.cs
@@ -3,13 +3,52 @@ using UnityEngine.UI;
 
 public class UISoundButton : MonoBehaviour
 {
-    private AudioSource _audioSource;
+    [SerializeField] private Sprite _soundOn;
+    [SerializeField] private Sprite _soundOff;
 
-    public void OnClick()
+    public GameObject SoundButton;
+
+    private int _soundIndex = 0;
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("soundIndex"))
+        {
+            StopSound();
+        }
+        else
+            PlayerPrefs.DeleteKey("soundIndex");
+        
+    }
+
+    public void AudioTumbler()
     {
         if (AudioListener.volume == 1)
-            AudioListener.volume = 0;
+        {
+            StopSound();
+            SaveSoundIndex();
+        }
         else
-            AudioListener.volume = 1;
+        {
+            PlaySound();
+            PlayerPrefs.DeleteKey("soundIndex");
+        }
+    }
+
+    private void PlaySound()
+    {
+        AudioListener.volume = 1;
+        SoundButton.GetComponent<Image>().sprite = _soundOn;
+    }
+
+    private void StopSound()
+    {
+        AudioListener.volume = 0;
+        SoundButton.GetComponent<Image>().sprite = _soundOff;
+    }
+
+    private void SaveSoundIndex()
+    {
+        PlayerPrefs.SetInt("soundIndex", _soundIndex);
     }
 }
